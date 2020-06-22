dc6deb15ae7b63755ba2f8c59bae643febe5ba19
Heondo Kim <heondokim@gmail.com>
2020-06-22 12:05:33 -0700

    not doing this yet

--- src/components/atoms/texts.js
+++ src/components/atoms/texts.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native'
 
 export const Text = styled.Text`
   color: ${(props) => props.theme.PRIMARY_TEXT_COLOR};
-  font-size: 16px;
+  font-size: ${(props) => (props.fontSize ? props.fontSize : '16px')};
   text-align: center;
   margin: ${(props) => (props.mg ? props.mg : '0')};
 `
--- src/components/molecules/ProfileBanner.js
+++ src/components/molecules/ProfileBanner.js
@@ -3,13 +3,26 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 // import auth from '@react-native-firebase/auth'
 import { ThemeProvider } from 'styled-components'
-
-import { Text, Row, Button, ButtonText, ProfileImage } from '../atoms'
+import styled from 'styled-components/native'
+import {
+  Text,
+  Div,
+  Row,
+  Button,
+  ButtonText,
+  ProfileImage,
+  EmptySpace,
+} from '../atoms'
 import { thunkLogout } from '../../state/auth/slice'
 import firebaseService from '../../services/firebase'
 
+const SignOutButton = styled.TouchableOpacity`
+  /* background-color: ${(props) => props.theme.PRIMARY_BUTTON_COLOR}; */
+  /* width: ${(props) => (props.width ? props.width : '100%')}; */
+`
+
 function ProfileBannerComponent({ authState, theme, thunkLogout }) {
-  const capialize = (str, lower = false) =>
+  const capitalize = (str, lower = false) =>
     (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
       match.toUpperCase(),
     )
@@ -28,15 +41,17 @@ function ProfileBannerComponent({ authState, theme, thunkLogout }) {
   return (
     <ThemeProvider theme={theme}>
       <Row>
-        <ProfileImage source={{ uri: authState.photoURL }} mg="0 6px" />
-        <Text>{capialize(authState.displayName)}</Text>
+        <ProfileImage source={{ uri: authState.photoURL }} mg="0 12px" />
+        <Div width="auto">
+          <Text fontSize="22px">{capitalize(authState.displayName)}</Text>
+          <SignOutButton onPress={handleLogout}>
+            <ButtonText>Sign Out</ButtonText>
+          </SignOutButton>
+        </Div>
         {/* <Button onPress={handleLogout}>
             <ButtonText>Sign Out</ButtonText>
           </Button> */}
       </Row>
-      <Button onPress={handleLogout} width="50%">
-        <ButtonText>Sign Out</ButtonText>
-      </Button>
     </ThemeProvider>
   )
 }
