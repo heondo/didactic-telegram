82b24d4d759ab8752d3aa88713bec159aec0955e
Heondo Kim <heondokim@gmail.com>
2020-06-23 21:04:23 -0700

    that was a lot easier than i thought it wold be with the select iamge library options

--- src/services/selectImage.js
+++ src/services/selectImage.js
@@ -2,11 +2,14 @@ import ImagePicker from 'react-native-image-picker'
 
 const options = {
   title: 'Select Image',
+  quality: 0.9,
+  maxWidth: 700,
+  maxHeight: 500,
   //   customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
-  storageOptions: {
-    skipBackup: true,
-    path: 'images',
-  },
+  // storageOptions: {
+  //   skipBackup: true,
+  //   path: 'images',
+  // },
 }
 
 const selectImageService = {
