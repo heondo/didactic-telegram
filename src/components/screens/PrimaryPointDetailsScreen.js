7dc1223546377c12d9964c6e64d585f117b6381a
Heondo Kim <heondokim@gmail.com>
2020-06-27 17:27:10 -0700

    remove tab view and replace with swiper, much better loading of dynamically generated tabs

--- src/components/screens/PrimaryPointDetailsScreen.js
+++ src/components/screens/PrimaryPointDetailsScreen.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 
 import { SafeAreaView, Text } from '../atoms'
+import MERIDIAN_POINTS_DATA from '../../shared/data/meridianPointsData'
 
 const PrimaryPointDetailsScreenComponent = ({ navigation, theme, pointID }) => {
+  const pointData = MERIDIAN_POINTS_DATA[pointID]
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <Text>{pointID}</Text>
+        <Text>{JSON.stringify(pointData)}</Text>
       </SafeAreaView>
     </ThemeProvider>
   )
--- src/components/screens/PrimaryPointDetailsTabs.js
+++ src/components/screens/PrimaryPointDetailsTabs.js
@@ -1,67 +1,21 @@
 import React, { useState } from 'react'
-import { Dimensions } from 'react-native'
 import { connect } from 'react-redux'
-import { TabView, TabBar } from 'react-native-tab-view'
 import { ThemeProvider } from 'styled-components/native'
-
 import { PrimaryPointDetailsScreen } from './PrimaryPointDetailsScreen'
 
-const RenderTabBar = (props) => {
-  const { theme } = props
-  return (
-    <TabBar
-      {...props}
-      indicatorStyle={{
-        backgroundColor: theme.TAB_INDICATOR,
-      }}
-      style={{
-        backgroundColor: theme.TAB_BACKGROUND,
-      }}
-    />
-  )
-}
+import Swiper from 'react-native-swiper'
 
-function PrimaryPointDetailsTabComponent({ navigation, route, theme }) {
+export const PrimaryPointDetailsTabs = ({ navigation, route, theme }) => {
   const { pointID, points } = route.params
-  const initialIndex = parseInt(pointID.split('-')[1], 0) - 1
-  const [index, setIndex] = useState(initialIndex)
-  const [routes] = useState(points.map((id) => ({ key: id, title: id })))
-
-  const renderScene = ({ route }) => {
-    return (
-      <PrimaryPointDetailsScreen navigation={navigation} pointID={route.key} />
-    )
-  }
-
-  const initialLayout = { width: Dimensions.get('window').width }
 
   return (
-    <ThemeProvider theme={theme}>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={initialLayout}
-        lazy={true}
-        renderTabBar={(props) => (
-          <RenderTabBar
-            {...props}
-            theme={theme}
-            tabStyle={{ width: 'auto' }}
-            scrollEnabled={true}
-          />
-        )}
-      />
-    </ThemeProvider>
+    <Swiper
+      showsButtons={true}
+      showsPagination={false}
+      index={parseInt(pointID.split('-')[1], 0) - 1}>
+      {points.map((p) => (
+        <PrimaryPointDetailsScreen key={p} pointID={p} />
+      ))}
+    </Swiper>
   )
 }
-
-const mapStateToProps = ({ theme }) => {
-  return {
-    theme,
-  }
-}
-
-export const PrimaryPointDetailsTabs = connect(mapStateToProps)(
-  PrimaryPointDetailsTabComponent,
-)
