dc93f770d840d71a337b895db684f68aa651f18c
Heondo Kim <heondokim@gmail.com>
2020-06-28 12:16:08 -0700

    work on adding note to firebase to test connection and everything else

--- src/components/screens/PrimaryPointDetailsScreen.js
+++ src/components/screens/PrimaryPointDetailsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
@@ -11,11 +11,20 @@ import {
   Row,
   ImageAbsolute,
   TextInput,
+  View,
 } from '../atoms'
 import MERIDIAN_POINTS_DATA from '../../shared/data/meridianPointsData'
 
-const PrimaryPointDetailsScreenComponent = ({ theme, authState, pointID }) => {
+const PrimaryPointDetailsScreenComponent = ({
+  theme,
+  authState,
+  userImages,
+  pointID,
+}) => {
   const pointData = MERIDIAN_POINTS_DATA[pointID]
+
+  const userPointData = userImages[pointID] || null
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView pd="0 0 8px 0">
@@ -33,25 +42,28 @@ const PrimaryPointDetailsScreenComponent = ({ theme, authState, pointID }) => {
           </Row>
         </Header>
         <EmptySpace />
-        <TextInput
-          multiline={true}
-          numberOfLines={6}
-          style={{ textAlignVertical: 'top' }}
-          maxLength={500}
-          placeholderTextColor={theme.GREY}
-          placeholder={
-            authState.isLoggedIn ? 'Add a note...' : 'Log in to save notes'
-          }
-          editable={authState.isLoggedIn ? true : false}
-        />
+        <View>
+          <TextInput
+            multiline={true}
+            numberOfLines={6}
+            style={{ textAlignVertical: 'top' }}
+            maxLength={500}
+            placeholderTextColor={theme.GREY}
+            placeholder={
+              authState.isLoggedIn ? 'Add a note...' : 'Log in to save notes'
+            }
+            editable={authState.isLoggedIn ? true : false}
+          />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   )
 }
-const mapStateToProps = ({ theme, authState }) => {
+const mapStateToProps = ({ theme, authState, userImages }) => {
   return {
     theme,
     authState,
+    userImages,
   }
 }
 PrimaryPointDetailsScreenComponent.propTypes = {
