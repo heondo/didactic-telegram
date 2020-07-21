184f08ee7aa70252cec3ccc0f3dded8675770a7b
Heondo Kim <heondokim@gmail.com>
2020-07-20 19:24:18 -0700

    ignore jest mocks

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,23 @@
  * @format
  */
 
+// import 'react-native'
+// import React from 'react'
+// import App from '../App'
+
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer'
+
+// it('renders correctly', () => {
+//   renderer.create(<App />)
+// })
 import 'react-native'
 import React from 'react'
 import App from '../App'
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-  renderer.create(<App />)
+test('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
