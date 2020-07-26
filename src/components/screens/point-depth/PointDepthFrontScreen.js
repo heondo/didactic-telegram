cb707b5dfacfb50f8c7e3b127640e67df307696c
Heondo Kim <heondokim@gmail.com>
2020-07-25 23:04:24 -0700

    it renders, and loads. but not zooming in on the component as expected

--- src/components/screens/point-depth/PointDepthFrontScreen.js
+++ src/components/screens/point-depth/PointDepthFrontScreen.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import Svg, { Image } from 'react-native-svg'
+import { Animated, StyleSheet } from 'react-native'
 
+import {
+  PanGestureHandler,
+  PinchGestureHandler,
+  RotationGestureHandler,
+  State,
+} from 'react-native-gesture-handler'
+import { View } from '../../atoms'
 const PointDepthFrontScreenComponent = ({ navigation, theme }) => {
   // biggest features
   /**
@@ -12,18 +20,51 @@ const PointDepthFrontScreenComponent = ({ navigation, theme }) => {
    *    3.a. Meridian Group, Color Code/Depth
    * 4. Click each point and view...details?
    */
+  // return (
+  //   <ThemeProvider theme={theme}>
+  //     <Svg height="100%" width="100%">
+  //       <Image
+  //         width="100%"
+  //         height="100%"
+  //         preserveAspectRatio="xMidYMid slice"
+  //         href={require('../../../shared/images/depth-images/front-core.png')}
+  //         clipPath="url(#clip)"
+  //       />
+  //     </Svg>
+  //   </ThemeProvider>
+  // )
+  const _baseScale = new Animated.Value(1)
+  const _pinchScale = new Animated.Value(1)
+  const _scale = Animated.multiply(_baseScale, _pinchScale)
+  let _lastScale = 1
+  const _onPinchGestureEvent = Animated.event(
+    [{ nativeEvent: { scale: _pinchScale } }],
+    { useNativeDriver: true },
+  )
+
+  const _onPinchHandlerStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      _lastScale *= event.nativeEvent.scale
+      _baseScale.setValue(_lastScale)
+      _pinchScale.setValue(1)
+    }
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Svg height="100%" width="100%">
-        <Image
-          width="100%"
-          height="100%"
-          preserveAspectRatio="xMidYMid slice"
-          href={require('../../../shared/images/depth-images/front-core.png')}
-          clipPath="url(#clip)"
+    <PinchGestureHandler
+      onGestureEvent={_onPinchGestureEvent}
+      onHandlerStateChange={_onPinchHandlerStateChange}>
+      <View collapsable={false}>
+        <Animated.Image
+          source={require('../../../shared/images/depth-images/front-core.png')}
+          style={[
+            {
+              transform: [{ perspective: 200 }, { scale: _scale }],
+            },
+          ]}
         />
-      </Svg>
-    </ThemeProvider>
+      </View>
+    </PinchGestureHandler>
   )
 }
 const mapStateToProps = ({ theme }) => {
