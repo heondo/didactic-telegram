9978b97f7bc7161414c17614ed29d0b026b4b6f1
Heondo Kim <heondokim@gmail.com>
2020-07-27 19:07:53 -0700

    so hard to find any help on testing react native for some reason? Resorting to checking for correct renders if I can correctly mock the redux store

--- __tests__/MemoryHomeScreen.test.js
+++ __tests__/MemoryHomeScreen.test.js
@@ -2,11 +2,43 @@ jest.useFakeTimers()
 
 import React from 'react'
 import 'react-native'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import renderer from 'react-test-renderer'
+import { ThemeProvider } from 'styled-components'
+
+import { lightTheme, darkTheme } from '../src/shared/theme'
 import { MemoryHomeScreen } from '../src/components/screens/memory'
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
 
 it('Memory Home Screen with list renders correctly', () => {
-  renderer.create(<MemoryHomeScreen />)
+  let store
+  beforeEach(() => {
+    store = mockStore({
+      theme: darkTheme,
+      authState: {
+        isLoading: false,
+        loadingMessage: '',
+        error: null,
+        loggedIn: false,
+        user: null,
+      },
+      userImages: {
+        isLoading: false,
+        loadingMessage: '',
+        error: null,
+        images: null,
+      },
+    })
+  })
+  renderer.create(
+    <Provider store={store}>
+      <MemoryHomeScreen />
+    </Provider>,
+  )
 })
