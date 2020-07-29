3d565c69b803af9c307419dcdb8edd53401c0d91
Heondo Kim <heondokim@gmail.com>
2020-07-28 20:54:48 -0700

    i am not understanding the panresponder, generate a box on the side or different page

--- __tests__/DetailsPointsListScreen.test.js
+++ __tests__/DetailsPointsListScreen.test.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-// import App, { Counter, dataReducer } from './App';
+import App, { Counter, dataReducer } from './App'
 
 const list = ['a', 'b', 'c']
 
 describe('App', () => {
   describe('Reducer', () => {
     it('should set a list', () => {
-      //   const state = { list: [], error: null }
-      //   const newState = dataReducer(state, {
-      //     type: 'SET_LIST',
-      //     list,
-      //   })
-      //   expect(newState).toEqual({ list, error: null })
+      const state = { list: [], error: null }
+      const newState = dataReducer(state, {
+        type: 'SET_LIST',
+        list,
+      })
+
+      expect(newState).toEqual({ list, error: null })
     })
 
-    // it('should reset the error if list is set', () => {
-    //   const state = { list: [], error: true };
-    //   const newState = dataReducer(state, {
-    //     type: 'SET_LIST',
-    //     list,
-    //   });
+    it('should reset the error if list is set', () => {
+      const state = { list: [], error: true }
+      const newState = dataReducer(state, {
+        type: 'SET_LIST',
+        list,
+      })
 
-    //   expect(newState).toEqual({ list, error: null });
-    // });
+      expect(newState).toEqual({ list, error: null })
+    })
 
-    // it('should set the error', () => {
-    //   const state = { list: [], error: null };
-    //   const newState = dataReducer(state, {
-    //     type: 'SET_ERROR',
-    //   });
+    it('should set the error', () => {
+      const state = { list: [], error: null }
+      const newState = dataReducer(state, {
+        type: 'SET_ERROR',
+      })
 
-    //   expect(newState.error).toBeTruthy();
-    // });
+      expect(newState.error).toBeTruthy()
+    })
   })
 })
--- src/components/screens/point-depth/PointDepthFrontScreen.js
+++ src/components/screens/point-depth/PointDepthFrontScreen.js
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
-import Svg, { Image } from 'react-native-svg'
+import Svg, { Image, G } from 'react-native-svg'
 import { View } from '../../atoms'
+import { PanResponder, Animated } from 'react-native'
 
 const PointDepthFrontScreenComponent = ({ theme }) => {
   // biggest features
@@ -14,17 +15,68 @@ const PointDepthFrontScreenComponent = ({ theme }) => {
    * 4. I need to add gesture handlers to this, and move the image around and change the scaling as i do so?
    * 4. Click each point and view...details?
    */
+  const position = new Animated.ValueXY()
+
+  const [imageState, setImageState] = useState({
+    zoom: 1,
+    left: 0, // max left and top is derived from scale
+    top: 0,
+  })
+
+  // const
+
+  // Zoom is determined by the distance from the initialX, initialY to the location on release
+  const _panResponder = useRef(
+    PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // We have a pinch-to-zoom movement
+        // Track locationX/locationY to know by how much the user moved their fingers
+        // console.log(evt.nativeEvent.locationX, evt.nativeEvent.locationY)
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        position.setValue({ x: gestureState.dx, y: gestureState.dy })
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {},
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+        // console.log('grant', evt, gestureState)
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true
+      },
+    }),
+  ).current
+
   return (
     <ThemeProvider theme={theme}>
-      <View height="100%" width="100%">
+      <View {..._panResponder.panHandlers}>
         <Svg height="100%" width="100%">
-          <Image
-            width="100%"
-            height="100%"
-            preserveAspectRatio="xMidYMid slice"
-            href={require('../../../shared/images/depth-images/front-core.png')}
-            clipPath="url(#clip)"
-          />
+          <G
+            transform={{
+              translateX: position.x,
+              translateY: position.y,
+              scale: imageState.zoom,
+            }}>
+            <Image
+              width="100%"
+              height="100%"
+              preserveAspectRatio="xMidYMid slice"
+              href={require('../../../shared/images/depth-images/front-core.png')}
+              clipPath="url(#clip)"
+            />
+          </G>
         </Svg>
       </View>
     </ThemeProvider>
