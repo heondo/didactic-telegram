860b1b3e77f4f8f16767b2fa71f9ee0aa8285649
Heondo Kim <heondokim@gmail.com>
2020-07-29 00:39:01 -0700

    i can, move an image around. Set boundaries for max offset and add zoom function

--- src/components/screens/point-depth/PointDepthBackScreen.js
+++ src/components/screens/point-depth/PointDepthBackScreen.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import Svg, { Image, G } from 'react-native-svg'
+import { Animated, PanResponder } from 'react-native'
+
+import { View, Text, Button } from '../../atoms'
 
 const PointDepthBackScreenComponent = ({ theme }) => {
-  return <ThemeProvider theme={theme}></ThemeProvider>
+  // useNativeDriver not specified?
+  const pan = useRef(new Animated.ValueXY()).current
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        })
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset()
+      },
+    }),
+  ).current
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Text>Drag this box!</Text>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}>
+        <Svg height="100%" width="100%">
+          <G>
+            <Image
+              width="100%"
+              height="100%"
+              preserveAspectRatio="xMidYMid slice"
+              href={require('../../../shared/images/depth-images/front-core.png')}
+              clipPath="url(#clip)"
+            />
+          </G>
+        </Svg>
+      </Animated.View>
+    </ThemeProvider>
+  )
 }
 const mapStateToProps = ({ theme }) => {
   return {
